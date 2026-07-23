0eb6e5f67abe3661d5ca78e78a4296ca06aeb6ec
Aditya Dhanawade <avdhanawade94@gmail.com>
2026-07-23 20:16:06 +0530

    Add splash screen and authentication (login, register, admin login/register)

---
+++ app/src/main/java/com/example/vehical_auto_app/adminregister.kt
@@ -0,0 +1,102 @@
+package com.example.vehical_auto_app
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
+class adminregister : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
+    var databaseReference: DatabaseReference? = null
+    var database: FirebaseDatabase? = null
+    var sharedpreferences: SharedPreferences? = null
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adminregister)
+
+        val name = findViewById<EditText>(R.id.edusername)
+
+        val number = findViewById<EditText>(R.id.ednumber)
+        val email = findViewById<EditText>(R.id.edemail)
+        val password = findViewById<EditText>(R.id.edpassword)
+        val btn = findViewById<Button>(R.id.btnregister)
+        auth = FirebaseAuth.getInstance()
+
+
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference!!.child("Admin")
+
+        btn.setOnClickListener {
+            if(name.text.isEmpty())
+            {
+                name.setError("Enter name")
+                return@setOnClickListener
+            }else if(password.text.isEmpty())
+            {
+                password.setError("Enter Password ")
+                return@setOnClickListener
+            }else if(number.text.isEmpty())
+            {
+                number.setError("Enter Contact Number")
+                return@setOnClickListener
+            }else if(email.text.isEmpty())
+            {
+                email.setError("Enter Email id")
+                return@setOnClickListener
+            }
+
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener {
+                    if(it.isSuccessful)
+                    {
+                        val currentuser = auth.currentUser
+                        val currentUserdb = databaseReference?.child((currentuser?.uid!!))
+                        currentUserdb?.child("name")?.setValue(name.text.toString())
+
+
+                        currentUserdb?.child("number")?.setValue(number.text.toString())
+
+                        Toast.makeText(applicationContext,"success", Toast.LENGTH_LONG).show()
+                        sharedata(email.text.toString(),name.text.toString())
+
+//                        Toast.makeText(applicationContext,adhar.text.toString(), Toast.LENGTH_LONG).show()
+
+ //                       val intent = Intent(applicationContext,adminlogin::class.java)
+  //                      startActivity(intent)
+                    }
+                    else
+                    {
+                        Toast.makeText(applicationContext,"failed", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
+    }
+
+    private fun sharedata(email: String, name: String) {
+
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences("My",Context.MODE_PRIVATE)
+        val editor:SharedPreferences.Editor =  sharedPreferences.edit()
+        editor.putString("email",email)
+        editor.putString("gname",name)
+        editor.apply()
+        editor.commit()
+
+
+
+    }
+}
